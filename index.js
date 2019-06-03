eeeaf959c11f016f7b7a744cb36ef48537759540
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-06-03 15:16:13 +0300

    solution

---
+++ index.js
@@ -0,0 +1,21 @@
+function isLeapYear(year) {
+    return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+}
+
+function daysInYear(year) {
+    try {
+        if ( !Number.isInteger(year) || !Number(year) > 0){
+            throw new Error();
+        }
+        console.log(isLeapYear(year) ? 366 : 365);
+    } catch (error) {
+        console.log('exception');
+    }
+}
+
+daysInYear(2019); // 365
+daysInYear('2019'); // exception
+daysInYear(2020); // 366
+daysInYear(2020.2); // exception
+daysInYear(2100); // 365
+daysInYear({year: 2100}); // exception
